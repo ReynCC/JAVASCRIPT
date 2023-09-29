656cbc32fb875a0fb0b9753e08aa687f4cd0e91b
Enzo Fernandez <94704882+ReynCC@users.noreply.github.com>
2023-09-29 19:47:01 -0300

    Update js.js

--- Examen nodejs/js.js
+++ Examen nodejs/js.js
@@ -1,50 +1,52 @@
 const http = require('http');
 const fs = require('fs');
 
-const mime = {
-  'html': 'text/html',
-  'css': 'text/css',
-  'jpg': 'image/jpg',
-  'ico': 'image/x-icon',
-  'mp3': 'audio/mpeg3',
-  'mp4': 'video/mp4'
-}
-
-//codigo del ejemplo.
-
 const servidor = http.createServer((pedido, respuesta) => {
-  const url = new URL('http://localhost:8888' + pedido.url)
-  let camino = 'public' + url.pathname
-  if (camino == 'public/')
-    camino = 'public/index.html'
-  encaminar(pedido, respuesta, camino)
-})
-
-servidor.listen(8888)
-
-function encaminar(){
+  const url = pedido.url;
   
-}
+  if (url === '/' || url === '/index.html') {
 
-console.log('Servidor creado');
+    respuesta.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream('index.html').pipe(respuesta);
+  } else if (url === '/traducir' && pedido.method === 'POST') {
+  
+    let datos = '';
+    
+    pedido.on('data', (chunk) => {
+      datos += chunk;
+    });
+    
+    pedido.on('end', () => {
 
-//(Función para cambiar idioma del texto) De alguna manera tengo que hacer que la función reciba 
-//el texto del formulario y muestre en pantalla el resultado de la traducción.
+      const fraseEnEspañol = decodeURIComponent(datos.split('=')[1]);
 
-/*function IdiomaP(texto) {
+      const fraseTraducida = traducirAlIdiomaP(fraseEnEspañol);
+
+      respuesta.writeHead(200, { 'Content-Type': 'text/html' });
+      respuesta.end(`${fraseTraducida}</p>`);
+    });
+  } else {
+
+  }
+});
+
+servidor.listen(8888, () => {
+  console.log('Servidor web iniciado en http://localhost:8888');
+});
+
+function traducirAlIdiomaP(frase) {
   const vocales = 'aeiouAEIOU';
-  let textoTraducido = '';
+  let fraseTraducida = '';
   
   for (let i = 0; i < frase.length; i++) {
     const letra = frase[i];
     
     if (vocales.includes(letra)) {
-      textoTraducido += letra + 'p' + letra.toLowerCase();
+      fraseTraducida += letra + 'p' + letra.toLowerCase();
     } else {
-      textoTraducido += letra;
+      fraseTraducida += letra;
     }
   }
   
-  return textoTraducido;
+  return fraseTraducida;
 }
-*/
